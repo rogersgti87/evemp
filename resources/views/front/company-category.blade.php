47b61fd27e21cd5cd5c4737ab69c5db1f6953047
Roger Guimaraes <rogersgti87@gmail.com>
2023-05-22 16:47:12 -0300

    220523

--- resources/views/front/company-category.blade.php
+++ resources/views/front/company-category.blade.php
@@ -10,7 +10,7 @@
                         <div class="card-body d-flex align-items-center">
                             <picture>
                                 <a href="#" data-toggle="modal" id="{{$cc->company_slug}}" data-nome="{{$cc->name}}" data-id="{{$cc->id}}" data-target="#modal-company">
-                                    <img style="border:1px solid #333; max-width:80px;" src="{{ $cc->image_thumb != null ? url("$cc->image_thumb") : url('assets/admin/img/thumb.png')}}" alt="{{$cc->name}}">
+                                    <img style="border:1px solid #333; max-width:80px;" src="{{ $cc->image_original != null ? url("$cc->image_original") : url('assets/admin/img/thumb.png')}}" alt="{{$cc->name}}">
                                 </a>
                             </picture>
                             <div class="flex-grow-1 content">
--- resources/views/front/modal-company.blade.php
+++ resources/views/front/modal-company.blade.php
@@ -66,7 +66,14 @@
 
           </div>
           <div class="tab-pane fade" id="custom-tabs-two-profile" role="tabpanel" aria-labelledby="custom-tabs-two-profile-tab">
-             Mauris tincidunt mi at erat gravida, eget tristique urna bibendum. Mauris pharetra purus ut ligula tempor, et vulputate metus facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas sollicitudin, nisi a luctus interdum, nisl ligula placerat mi, quis posuere purus ligula eu lectus. Donec nunc tellus, elementum sit amet ultricies at, posuere nec nunc. Nunc euismod pellentesque diam.
+            <div class="card card-danger">
+                <div class="card-header">
+                <h4 class="card-title">Localização</h4>
+                </div>
+                <div class="card-body">
+                    {!! $company->google_maps !!}
+                </div>
+                </div>
           </div>
         </div>
       </div>
